e100181a82146c6eb9d95c6f40dc1c4129d4f62e
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-22 14:44:08 -0400

    Added some methods for knights_travails.rv

--- poly_tree_node/lib/knights_travails.rb
+++ poly_tree_node/lib/knights_travails.rb
@@ -1,21 +1,36 @@
 require_relative "00_tree_node.rb"
 
 class KnightPathFinder
-    attr_reader :pos
+    attr_reader :starting_pos
 
-    def initialize(pos)
-        @pos = pos
-        @root_node = PolyTreeNode.new(pos)
-        @considered_pos = [pos]
+    def initialize(starting_pos)
+        @starting_pos = starting_pos
+        @root_node = PolyTreeNode.new(starting_pos)
+        @considered_pos = [starting_pos]
     end
 
     def self.valid_moves(pos)
+        array = []
+        row, col = pos
 
+        array << [row + 1, col + 2]
+        array << [row + 1, col - 2]
+        array << [row - 1, col + 2]
+        array << [row - 1, col - 2]
+        array << [row + 2, col + 1]
+        array << [row + 2, col - 1]
+        array << [row - 2, col + 1]
+        array << [row - 2, col - 1]
+
+        array.select do |pos|
+            row, col = pos 
+            (row >= 0 && row < 8) && (col >= 0 && col < 8)
+        end
     end
 
 
     def new_move_positions(pos)
-        @considered_pos.select {|pos| self.valid_moves(pos)}
+        #@considered_pos.select {|pos| self.valid_moves(pos)}
     end
 
   
