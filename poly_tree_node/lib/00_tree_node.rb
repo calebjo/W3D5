d6e58279bdd7a399008a155091f0f91377d65c03
tasangpo <tashi.rayshar@gmail.com>
2021-10-22 11:52:17 -0400

    Polytree #initialize, #parent, #children, #value, #parent=, #add_child, #remove_child

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -1,4 +1,7 @@
 class PolyTreeNode
+
+    attr_accessor :parent, :children, :value
+
     def initialize(value)
         @parent = nil
         @children = []
@@ -17,11 +20,26 @@ class PolyTreeNode
         @value
     end
 
-    def parent=(node)
-        if !node.nil? && !node.parent.nil?
+    def parent=(node) 
+        if !node.nil?
             node.children << self if !node.children.include?(self)
-            node.parent.children.pop(node)
+            # node.parent.children.delete(node) if !node.parent.nil?
         end
         @parent = node
     end
+
+    def add_child(node)
+        node.parent = self
+        children << node if !children.include?(node)
+    end
+
+    def remove_child(node)
+        if !self.children.include?(node)
+            raise Exception.new("Node is not a child")
+        end
+        node.parent = nil
+        children.delete(node)
+    end
+
+
 end
