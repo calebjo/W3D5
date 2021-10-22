bd84c991ade85206d4ba4d452975d9f06ae2618c
Caleb Jones <caleb.ri.jones@gmail.com>
2021-10-22 11:29:07 -0400

    Added PolyTreeNode class

--- poly_tree_node/lib/00_tree_node.rb
+++ poly_tree_node/lib/00_tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
+    def initialize(value)
+        @parent = nil
+        @children = []
+        @value = value
+    end
 
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def value
+        @value
+    end
+
+    def parent=(node)
+        if !node.nil? && !node.parent.nil?
+            node.children << self if !node.children.include?(self)
+            node.parent.children.pop(node)
+        end
+        @parent = node
+    end
 end
